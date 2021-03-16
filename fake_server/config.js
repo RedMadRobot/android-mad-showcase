d5996e3eff490e00e1cc5d43c9f94df4c8c31c29
av.egorov <av.egorov@redmadrobot.com>
2021-03-16 12:33:29 +0400

    [Network] fix port

--- fake_server/config.js
+++ fake_server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.SERVER_PORT || 5000,
-    accounts_port: process.env.SERVER_ACCOUNTS_PORT || 5001
+    port: process.env.PORT || 5000,
+    accounts_port: process.env.ACCOUNTS_PORT || 5001
 }
