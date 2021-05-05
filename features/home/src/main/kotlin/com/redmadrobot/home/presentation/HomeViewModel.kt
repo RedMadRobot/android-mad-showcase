c3ae1988a4fd1e44b810aebcc8035feb1fa3c3b0
avegrv <avegrv@gmail.com>
2021-05-05 17:50:31 +0400

    [Home] Added LCE implementation for home screen (#9)
    * [Home] Added LCE implementation for home screen
    
    * [Home] Fix naming for HomeViewState
    
    * [Home] Implement stub
    
    * [Home] Fix cards nullability
    
    Co-authored-by: av.egorov <av.egorov@redmadrobot.com>

--- features/home/src/main/kotlin/com/redmadrobot/home/presentation/HomeViewModel.kt
+++ features/home/src/main/kotlin/com/redmadrobot/home/presentation/HomeViewModel.kt
@@ -1,8 +1,45 @@
 package com.redmadrobot.home.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.redmadrobot.core.extensions.safeLaunch
+import com.redmadrobot.core_network.ApolloApi
+import com.redmadrobot.core_network.CardsListQuery
+import com.redmadrobot.core_network.exception.NetworkException
+import com.redmadrobot.core_presentation.extensions.update
+import com.redmadrobot.core_presentation.model.Content
+import com.redmadrobot.core_presentation.model.Loading
+import com.redmadrobot.core_presentation.model.Stub
+import com.redmadrobot.home.presentation.state.HomeViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel()
+class HomeViewModel @Inject constructor(
+    private val api: ApolloApi
+) : ViewModel() {
+
+    private val _viewState = MutableStateFlow(HomeViewState(cardsState = Loading()))
+    val viewState: StateFlow<HomeViewState> = _viewState
+
+    init {
+        loadCards()
+    }
+
+    fun onRetryClicked() = loadCards()
+
+    private fun loadCards() {
+        _viewState.update { copy(cardsState = Loading()) }
+        viewModelScope.safeLaunch(
+            {
+                val cards = api.query(CardsListQuery()).cards
+                _viewState.update { copy(cardsState = Content(cards)) }
+            },
+            onError = { throwable ->
+                _viewState.update { copy(cardsState = Stub(throwable)) }
+            }
+        )
+    }
+}
---
+++ features/home/src/main/kotlin/com/redmadrobot/home/presentation/state/HomeViewState.kt
@@ -0,0 +1,9 @@
+package com.redmadrobot.home.presentation.state
+
+import com.redmadrobot.core_network.CardsListQuery.Card
+import com.redmadrobot.core_presentation.model.State
+
+data class HomeViewState(
+   val title: String = "Cards List",
+   val cardsState: State<List<Card>>
+)
