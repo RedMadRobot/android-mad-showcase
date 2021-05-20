fd3f4dd576136f24c01bbb9bad2bec433eed85ca
p.gabrusevich <p.gabrusevich@redmadrobot.com>
2021-05-21 00:35:56 +0400

    Отрисовка

--- features/home/src/main/kotlin/com/redmadrobot/home/presentation/HomeScreen.kt
+++ features/home/src/main/kotlin/com/redmadrobot/home/presentation/HomeScreen.kt
@@ -1,77 +1,146 @@
 package com.redmadrobot.home.presentation
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.redmadrobot.core_network.CardsListQuery.Card
 import com.redmadrobot.core_presentation.model.Content
 import com.redmadrobot.core_presentation.model.Loading
 import com.redmadrobot.core_presentation.model.State
 import com.redmadrobot.core_presentation.model.Stub
 import com.redmadrobot.home.R
-import kotlin.math.roundToInt
-import kotlin.math.sign
+import com.redmadrobot.home.presentation.model.CardViewInfoUi
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
     val viewState = viewModel.viewState.collectAsState().value
     Column {
-        Text(text = viewState.title, color = Color.White)
-        CardsList(viewState.cardsState, viewModel = viewModel) { viewModel.onRetryClicked() }
+        HomeContent(viewState.cardsState) { viewModel.onRetryClicked() }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
+fun Modifier.drawTextColoredShadow(
+    shadowColor: Color,
+    textColor: Color,
+    alpha: Float = 0.8f,
+    shadowRadius: Float = 5f,
+    offsetY: Float = 3f,
+    offsetX: Float = 3f,
+    text: String
+) = this.drawBehind {
+    val shadowColorArgb =
+        android.graphics.Color.toArgb(shadowColor.copy(alpha = alpha).value.toLong())
+    val textColorArgb = android.graphics.Color.toArgb(textColor.value.toLong())
+
+    drawIntoCanvas {
+        val paint = Paint()
+        val frameworkPaint = paint.asFrameworkPaint()
+        frameworkPaint.isAntiAlias = true
+        frameworkPaint.color = textColorArgb
+        frameworkPaint.textSize = 50f
+        frameworkPaint.setShadowLayer(
+            shadowRadius,
+            offsetX,
+            offsetY,
+            shadowColorArgb
+        )
+        it.nativeCanvas.drawText(text, 0f, this.size.height, frameworkPaint)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun Modifier.drawColoredShadow(
+    color: Color,
+    darkColor: Color,
+    alpha: Float = 0.2f,
+    borderRadius: Dp = 20.dp,
+    shadowRadius: Dp = 10.dp,
+    offsetY: Dp = 0.dp,
+    offsetX: Dp = 0.dp,
+    offsetDarkY: Dp = 0.dp,
+    offsetDarkX: Dp = 0.dp
+) = this.drawBehind {
+    val transparentColor = android.graphics.Color.toArgb(color.copy(alpha = 0.0f).value.toLong())
+
+    val shadowColor = android.graphics.Color.toArgb(color.copy(alpha = alpha).value.toLong())
+    val shadowDarkColor =
+        android.graphics.Color.toArgb(darkColor.copy(alpha = alpha).value.toLong())
+    this.drawIntoCanvas {
+        val paint = Paint()
+        val frameworkPaint = paint.asFrameworkPaint()
+        frameworkPaint.color = transparentColor
+        frameworkPaint.setShadowLayer(
+            shadowRadius.toPx(),
+            offsetX.toPx(),
+            offsetY.toPx(),
+            shadowColor
+        )
+        it.drawRoundRect(
+            0f,
+            0f,
+            this.size.width - 2 * shadowRadius.value,
+            this.size.height - 2 * shadowRadius.value,
+            borderRadius.toPx(),
+            borderRadius.toPx(),
+            paint
+        )
+        val darkPaint = Paint()
+        val frameworkDarkPaint = darkPaint.asFrameworkPaint()
+        frameworkDarkPaint.setShadowLayer(
+            shadowRadius.toPx(),
+            offsetDarkX.toPx(),
+            offsetDarkY.toPx(),
+            shadowDarkColor
+        )
+        it.drawRoundRect(
+            2 * shadowRadius.value,
+            0f,
+            this.size.width - 2 * shadowRadius.value,
+            this.size.height,
+            borderRadius.toPx(),
+            borderRadius.toPx(),
+            darkPaint
+        )
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnrememberedMutableState")
 @Composable
-private fun CardContent(item: Card, viewModel: HomeViewModel, cardIndex: Int) {
+private fun CardsList(item: CardViewInfoUi) {
     val height = 200.dp
     val context = LocalContext.current
-    val cardBackground = viewModel.determineCardGradient(item.color)
-    val iconTint = viewModel.determineIconColor(item.color)
-    val icon = viewModel.determineIcon(item.type)
-    var isPressed by remember { mutableStateOf(true) }
-    var newValue: Int
-    var offsetY by remember { mutableStateOf(0f) }
     Column(
         Modifier
-            .offset { IntOffset(0, offsetY.roundToInt()) }
-            .padding(10.dp)
-            .draggable(
-                orientation = Orientation.Vertical,
-                state = rememberDraggableState { delta ->
-                    if (isPressed) {
-                        val summed = delta + offsetY
-                        newValue = summed.coerceIn(-1f, 1f).roundToInt()
-                        if (offsetY.sign > 0 && offsetY == 20f) {
-                            viewModel.onTouchDown(cardIndex)
-                        } else if (offsetY.sign < 0 && offsetY == -20f) {
-                            viewModel.onTouchUp(cardIndex)
-                        }
-                        offsetY += newValue
-                    }
-                },
-                onDragStopped = {
-                    isPressed = false
-                    newValue = 0
-                    offsetY = 0f
-                },
-                onDragStarted = {
-                    isPressed = true
-                }
+            .padding(horizontal = 10.dp, vertical = 10.dp)
+            .drawColoredShadow(
+                Color.White,
+                darkColor = Color.Black,
+                borderRadius = 10.dp,
+                offsetY = 0.dp,
+                offsetX = 0.dp,
+                offsetDarkY = 10.dp
             )
             .clickable {
                 Toast
@@ -81,36 +150,47 @@ private fun CardContent(item: Card, viewModel: HomeViewModel, cardIndex: Int) {
             .fillMaxWidth()
             .height(height)
             .clip(RoundedCornerShape(10.dp))
-            .background(cardBackground)
+            .background(item.cardGradient)
+            .border(BorderStroke(1.dp, item.cardBorderGradient), RoundedCornerShape(10.dp))
     ) {
         Icon(
             modifier = Modifier
                 .padding(30.dp),
             painter = painterResource(id = R.drawable.ic_subtract),
             contentDescription = "substract",
-            tint = iconTint,
+            tint = item.iconColor,
         )
+        Spacer(modifier = Modifier.height(40.dp))
         Row(
             Modifier
-                .padding(30.dp)
+                .padding(horizontal = 30.dp)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = item.number, color = Color.White)
+            Text(
+                modifier = Modifier
+                    .align(Alignment.Bottom)
+                    .drawTextColoredShadow(
+                        shadowColor = Color.White,
+                        textColor = item.iconColor,
+                        text = item.number
+                    ), color = Color.Black, text = ""
+            )
             Icon(
-                painter = painterResource(id = icon),
+                modifier = Modifier.align(Alignment.Bottom),
+                painter = painterResource(id = item.iconRes),
                 contentDescription = "type",
-                tint = iconTint
+                tint = item.iconColor
             )
         }
     }
 
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-private fun CardsList(
-    cardsState: State<List<Card>>,
-    viewModel: HomeViewModel,
+private fun HomeContent(
+    cardsState: State<List<CardViewInfoUi>>,
     onRetryClickListener: () -> Unit,
 ) {
     when (cardsState) {
@@ -119,10 +199,17 @@ private fun CardsList(
         }
         is Content -> {
             val cards = cardsState.content
-            LazyColumn(reverseLayout = true, verticalArrangement = Arrangement.spacedBy(-138.dp)) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(vertical = 50.dp, horizontal = 14.dp)
+                    .background(Color(0xFF434A74))
+                    .clip(
+                        RoundedCornerShape(10.dp)
+                    ), reverseLayout = true, verticalArrangement = Arrangement.spacedBy(-90.dp)
+            ) {
                 items(count = cards.size) { cardIndex ->
                     val card = cards[cardIndex]
-                    CardContent(item = card, viewModel = viewModel, cardIndex = cardIndex)
+                    CardsList(item = card)
                 }
             }
         }
--- features/home/src/main/kotlin/com/redmadrobot/home/presentation/HomeViewModel.kt
+++ features/home/src/main/kotlin/com/redmadrobot/home/presentation/HomeViewModel.kt
@@ -1,8 +1,5 @@
 package com.redmadrobot.home.presentation
 
-import CardGradient
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.redmadrobot.core.extensions.safeLaunch
@@ -26,9 +23,6 @@ class HomeViewModel @Inject constructor(
     private val _viewState = MutableStateFlow(HomeViewState(cardsState = Loading()))
     val viewState: StateFlow<HomeViewState> = _viewState
 
-    private val swipeCards = mutableListOf<CardsListQuery.Card>()
-    var isPressed = true
-
     init {
         loadCards()
     }
@@ -40,70 +34,11 @@ class HomeViewModel @Inject constructor(
         viewModelScope.safeLaunch(
             {
                 val cards = api.query(CardsListQuery()).cards
-                _viewState.update { copy(cardsState = Content(cards)) }
-                _viewState.update { copy(cards = cards) }
+                _viewState.update { copy(cardsState = Content(cards.map {it.toCardViewInfoUi()})) }
             },
             onError = { throwable ->
                 _viewState.update { copy(cardsState = Stub(throwable)) }
             }
         )
-    }
-
-    fun onTouchUp(index: Int) {
-        val lastIndex = _viewState.value.cards?.lastIndex
-        val cards = _viewState.value.cards?.filterIndexed { i, _ ->
-            i == lastIndex || i <= index
-        }
-        if (cards != null) {
-            val removedItems = _viewState.value.cards?.filter { it !in cards }
-            removedItems?.let { swipeCards.addAll(it) }
-            _viewState.update {
-                copy(cardsState = Content(cards))
-            }
-            _viewState.update { copy(cards = cards) }
-        }
-        isPressed = false
-    }
-
-    fun onTouchDown(index: Int) {
-        val cards = _viewState.value.cards?.flatMapIndexed { i: Int, card: CardsListQuery.Card ->
-            if (i == index) listOf(card) + swipeCards else listOf(card)
-        }
-        if (cards!=null) {
-            swipeCards.clear()
-            _viewState.update { copy(cardsState = Content(cards)) }
-            _viewState.update { copy(cards = cards) }
-        }
-        isPressed = false
-    }
-
-    fun determineCardGradient(color: String): Brush {
-        return when (color) {
-            "pink" -> CardGradient.PINK.brush
-            "blue" -> CardGradient.BLUE.brush
-            "red" -> CardGradient.RED.brush
-            "yellow" -> CardGradient.YELLOW.brush
-            "green" -> CardGradient.GREEN.brush
-            else -> CardGradient.YELLOW.brush
-        }
-    }
-
-    fun determineIconColor(color: String): Color {
-        return when (color) {
-            "pink" -> Color(0xFF8D7CE7)
-            "blue" -> Color(0xFF6D94DA)
-            "red" -> Color(0xEECF5942)
-            "yellow" -> Color(0xFFDB8A3F)
-            "green" -> Color(0xFF539A83)
-            else -> Color(0xFF539A83)
-        }
-    }
-
-    fun determineIcon(type: String): Int {
-        return when (type) {
-            "VISA" -> TypeIconCard.VISA.icon
-            "MASTER_CARD" -> TypeIconCard.MASTERCARD.icon
-            else -> TypeIconCard.MASTERCARD.icon
-        }
     }
 }
