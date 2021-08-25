ce22d3568d39ef25feeac72d3e310d9bc3e07cf2
avegrv <avegrv@gmail.com>
2021-08-25 15:13:59 +0400

    Migrate to 31 API

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,7 +1,7 @@
 object AndroidConfig {
     const val MIN_SDK_VERSION = 26
-    const val COMPILE_SDK_VERSION = 30
-    const val TARGET_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 31
+    const val TARGET_SDK_VERSION = 31
 }
 
 interface BuildType {
