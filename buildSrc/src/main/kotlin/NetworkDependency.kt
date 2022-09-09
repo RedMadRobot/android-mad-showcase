d62ce92460a8aa9d86647002337682d2404761b2
Sasha Egorov <avegrv@gmail.com>
2022-09-09 21:15:31 +0100

    dependencies: update network

--- buildSrc/src/main/kotlin/NetworkDependency.kt
+++ buildSrc/src/main/kotlin/NetworkDependency.kt
@@ -1,7 +1,8 @@
 private object NetworkVersions {
+    const val APOLLO = CoreVersions.APOLLO
 }
 
 object NetworkDependency {
-    const val APOLLO_GRAPHQL = "com.apollographql.apollo:apollo-runtime:${CoreVersions.APOLLO}"
-    const val APOLLO_COROUTINES = "com.apollographql.apollo:apollo-coroutines-support:${CoreVersions.APOLLO}"
+    const val APOLLO_GRAPHQL = "com.apollographql.apollo:apollo-runtime:${NetworkVersions.APOLLO}"
+    const val APOLLO_COROUTINES = "com.apollographql.apollo:apollo-coroutines-support:${NetworkVersions.APOLLO}"
 }
