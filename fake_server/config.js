95cac4fc5b65721804caa46479aeb479e31a9d77
Sasha Egorov <avegrv@gmail.com>
2023-02-11 19:14:09 +0000

    change port location

--- fake_server/config.js
+++ fake_server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 5000,
-    accounts_port: process.env.ACCOUNTS_PORT || 5001
+    port: process.env.PORT || 6000,
+    accounts_port: process.env.ACCOUNTS_PORT || 6001
 }
